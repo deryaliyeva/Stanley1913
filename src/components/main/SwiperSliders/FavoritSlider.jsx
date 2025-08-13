e352e46427a38c0be5668be23b3558ff6d485a0b
deryaliyeva <dryaliyeva8@gmail.com>
2025-08-13 06:21:52 +0400

    Vercel-deploy-error

--- src/components/main/SwiperSliders/FavoritSlider.jsx
+++ src/components/main/SwiperSliders/FavoritSlider.jsx
@@ -4,7 +4,7 @@ import { Pagination } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
-import brands from "../data/brandData";
+import brands from "../data/BrandData";
 
 function FavoritSlider() {
     return (
